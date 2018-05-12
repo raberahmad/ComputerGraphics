4ed414d42b93b905fddc379b4043b47241cfc8de
DESKTOP-4ONSGSD\Raber <raberhaw>
2018-05-12 16:51:52 +0200

    some changes

--- RGB-HSL/src/App.java
+++ RGB-HSL/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-
+        ColorConverter converter = new ColorConverter();
+        converter.RGBtoHSL(5, 200, 0);
     }
 }
--- RGB-HSL/src/ColorConverter.java
+++ RGB-HSL/src/ColorConverter.java
@@ -1,6 +1,29 @@
 public class ColorConverter {
     public void RGBtoHSL(int R, int G, int B){
-        int[] values = {R, G, B};
+        double r = R / 255.0;
+        double g = G / 255.0;
+        double b = B / 255.0;
+
+        double Cmax;
+        double Cmin;
+
+        if ((r > g) && (b < g) ){
+            Cmax = r;
+            Cmin = b;
+
+            double H = (g - Cmin)/(Cmax - Cmin)*60.0;
+            System.out.println(H);
+        }
+        if ((r < g) && (b < r) ){
+            Cmax = g;
+            Cmin = b;
+
+            double H = (Cmax - r)/(Cmax-Cmin)*60.0+60.0;
+            System.out.println(H);
+        }
+        else {
+            System.out.println("error");
+        }
         
     }
 }
