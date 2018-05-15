5efc5a4871a39ebae5aff86c491cc78fbb1bb06d
Unknown <raberhawler@hotmail.com>
2018-05-15 20:48:16 +0200

    laaste fixjes rgb naar hsl

--- RGB-HSL/src/App.java
+++ RGB-HSL/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         ColorConverter converter = new ColorConverter();
-        converter.RGBtoHSL(100 , 100, 100);
+        converter.RGBtoHSL(100 , 110, 1);
     }
 }
--- RGB-HSL/src/ColorConverter.java
+++ RGB-HSL/src/ColorConverter.java
@@ -10,67 +10,52 @@ public class ColorConverter {
         double L=0.0;
 
         if ((r >= g) && (b <= g) ){
-
             Cmax = r;
-                Cmin = b;
+            Cmin = b;
             H = (g - Cmin)/(Cmax - Cmin)*60.0;
-
         }
         else if ((r <= g) && (b <= r) ){
                 Cmax = g;
                 Cmin = b;
-            H = (Cmax - r)/(Cmax-Cmin)*60.0+60.0;
+                H = (Cmax - r)/(Cmax-Cmin)*60.0+60.0;
         }
         else if((g >= b) && (r <= b)){
                 Cmax = g;
                 Cmin= r;
-
-            H = (b-Cmin)/(Cmax-Cmin)*60.0+120.0;
+                H = (b-Cmin)/(Cmax-Cmin)*60.0+120.0;
         }
         else if((b >= g) && (g >= r)){
                 Cmax = b;
                 Cmin = r;
-
-            H = (Cmax - g)/(Cmax-Cmin)*60.0+180.0;
-
+                H = (Cmax - g)/(Cmax-Cmin)*60.0+180.0;
         }
         else if((b >= r) && (g <= r)){
                 Cmax = b;
                 Cmin = g;
-
-            H = (r -Cmin)/(Cmax - Cmin)*60.0+240.0;
-
+                H = (r -Cmin)/(Cmax - Cmin)*60.0+240.0;
         }
         else if((r >= b) && (g <= b)){
                 Cmax =r;
                 Cmin = g;
-
-
-            H = (Cmax - b)/(Cmax-Cmin)*60.0+300.0;
-        }
-
-        else {
-            System.out.println("error");
+                H = (Cmax - b)/(Cmax-Cmin)*60.0+300.0;
         }
 
         if (r == g && g ==b){
             H=0.0;
             S=0.0;
+            Cmax = r;
+            Cmin= r;
 
             System.out.println("H:\tis niet gedefiniëerd");
-
-            Cmax = r;
-            Cmin=r;
         }
         else {
-            System.out.println("H:\t"+H);
+            System.out.println("H:\t"+H+"\u00b0");
             S = (Cmax-Cmin)/(1.0-Math.abs((Cmin+Cmax)-1.0));
         }
-        System.out.println("S:\t"+S);
 
         L = (Cmin + Cmax)/2.0;
-        System.out.println("L:\t"+L);
 
-        
+        System.out.println("S:\t"+S+"\tof in procenten "+S*100.0+"%");
+        System.out.println("L:\t"+L+"\tof in procenten "+L*100.0+"%");
     }
 }
