1093c3588192a705a2a077b57aa5d906ea3b114b
Unknown <raberhawler@hotmail.com>
2018-05-15 18:12:11 +0200

    finished rgb to hsh

--- RGB-HSL/src/App.java
+++ RGB-HSL/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         ColorConverter converter = new ColorConverter();
-        converter.RGBtoHSL(5, 200, 0);
+        converter.RGBtoHSL(100 , 100, 100);
     }
 }
--- RGB-HSL/src/ColorConverter.java
+++ RGB-HSL/src/ColorConverter.java
@@ -3,27 +3,74 @@ public class ColorConverter {
         double r = R / 255.0;
         double g = G / 255.0;
         double b = B / 255.0;
+        double Cmax=0.0 ;
+        double Cmin=0.0;
+        double H=0.0;
+        double S=0.0;
+        double L=0.0;
 
-        double Cmax;
-        double Cmin;
+        if ((r >= g) && (b <= g) ){
 
-        if ((r > g) && (b < g) ){
             Cmax = r;
-            Cmin = b;
+                Cmin = b;
+            H = (g - Cmin)/(Cmax - Cmin)*60.0;
 
-            double H = (g - Cmin)/(Cmax - Cmin)*60.0;
-            System.out.println(H);
         }
-        if ((r < g) && (b < r) ){
-            Cmax = g;
-            Cmin = b;
+        else if ((r <= g) && (b <= r) ){
+                Cmax = g;
+                Cmin = b;
+            H = (Cmax - r)/(Cmax-Cmin)*60.0+60.0;
+        }
+        else if((g >= b) && (r <= b)){
+                Cmax = g;
+                Cmin= r;
 
-            double H = (Cmax - r)/(Cmax-Cmin)*60.0+60.0;
-            System.out.println(H);
+            H = (b-Cmin)/(Cmax-Cmin)*60.0+120.0;
         }
+        else if((b >= g) && (g >= r)){
+                Cmax = b;
+                Cmin = r;
+
+            H = (Cmax - g)/(Cmax-Cmin)*60.0+180.0;
+
+        }
+        else if((b >= r) && (g <= r)){
+                Cmax = b;
+                Cmin = g;
+
+            H = (r -Cmin)/(Cmax - Cmin)*60.0+240.0;
+
+        }
+        else if((r >= b) && (g <= b)){
+                Cmax =r;
+                Cmin = g;
+
+
+            H = (Cmax - b)/(Cmax-Cmin)*60.0+300.0;
+        }
+
         else {
             System.out.println("error");
         }
+
+        if (r == g && g ==b){
+            H=0.0;
+            S=0.0;
+
+            System.out.println("H:\tis niet gedefiniëerd");
+
+            Cmax = r;
+            Cmin=r;
+        }
+        else {
+            System.out.println("H:\t"+H);
+            S = (Cmax-Cmin)/(1.0-Math.abs((Cmin+Cmax)-1.0));
+        }
+        System.out.println("S:\t"+S);
+
+        L = (Cmin + Cmax)/2.0;
+        System.out.println("L:\t"+L);
+
         
     }
 }
